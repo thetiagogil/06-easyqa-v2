2fdee11ab672e032aedfb76938cdf8063a7877ce
thetiagogil <talmeidagil@hotmail.com>
2025-02-27 21:17:33 +0000

    configure frontend

---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,10 @@
+import "@/styles/globals.css";
+import { Stack, Typography } from "@mui/joy";
+
+export default function HomePage() {
+  return (
+    <Stack>
+      <Typography>hello world</Typography>
+    </Stack>
+  );
+}
